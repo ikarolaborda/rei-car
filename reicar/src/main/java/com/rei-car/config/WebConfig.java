a744701d6ff4412ec2f01ce844dcef547ca493c1
JVFrancaLisboa <logiccoder88@gmail.com>
2026-01-21 10:33:55 -0300

    Project Estructured

---
+++ reicar/src/main/java/com/rei-car/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.ideasystem.config;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.beans.PropertyEditorSupport;
+import java.math.BigDecimal;
+
+@ControllerAdvice
+public class WebConfig {
+
+    // Conversor de moeda
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(BigDecimal.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                if (text == null || text.trim().isEmpty()) {
+                    setValue(null);
+                    return;
+                }
+
+                try {
+                    String formatado = text.trim();
+
+                    if (formatado.contains(",")) {
+                        formatado = formatado.replace(".", "").replace(",", ".");
+                    }
+
+                    setValue(new BigDecimal(formatado));
+                } catch (Exception e) {
+                    setValue(BigDecimal.ZERO);
+                }
+            }
+        });
+    }
+
+}
