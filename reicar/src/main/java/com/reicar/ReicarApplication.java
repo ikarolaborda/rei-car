7a79748b207cbe694e1bffdf5bcb055bad430577
JVFrancaLisboa <logiccoder88@gmail.com>
2026-01-21 21:06:29 -0300

    feat: dashboard and initial functions

---
+++ reicar/src/main/java/com/reicar/ReicarApplication.java
@@ -0,0 +1,13 @@
+package com.reicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReicarApplication.class, args);
+	}
+
+}
--- reicar/src/test/java/com/reicar/ApplicationTests.java
+++ reicar/src/test/java/com/reicar/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ideasystem;
+package com.reicar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
